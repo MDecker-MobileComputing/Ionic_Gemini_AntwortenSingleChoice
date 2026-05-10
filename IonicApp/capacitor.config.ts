568cb64bb79872dfaac565125a5478bfc4f7c637
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-10 16:56:11 +0200

    Unmittelbar nach Erzeugung Ionic-Projekt

---
+++ IonicApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.eldecker.ionic.antwortensinglechoice',
+  appName: 'Gemini: SC-Antworten',
+  webDir: 'www'
+};
+
+export default config;
